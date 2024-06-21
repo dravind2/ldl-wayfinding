20111403a749136bb7f2ad04384852836b93dfe7
Divya Ravindra <89780095+dravind2@users.noreply.github.com>
2024-06-20 20:22:34 -0500

    put secret key back

--- wayfinding_api/settings.py
+++ wayfinding_api/settings.py
@@ -19,6 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-^66bs1kcmm&w169a@*_#qhmx1wsya*^b#8&6vo^mwz4m&@e!4)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
