163a77fb7e22450c206b8d9507899acbbe81e6e2
Divya Ravindra <89780095+dravind2@users.noreply.github.com>
2024-06-28 09:24:58 -0500

    sample data

--- example1/views.py
+++ example1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from rest_framework.permissions import AllowAny
+import logging
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
+
+class HelloWorld(APIView):
+    @csrf_exempt
+    def post(self, _):
+        return JsonResponse({'message' : 'Hello, success!'}, status=200)
--- wayfinding_api/settings.py
+++ wayfinding_api/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'corsheaders',
     'django_filters',
+    'rest_framework',
+    'rest_framework_simplejwt',
     
 ]
 
