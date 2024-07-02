d7951e2dfe361014064082e1f6e572e51564b18b
Divya Ravindra <89780095+dravind2@users.noreply.github.com>
2024-07-01 21:23:49 -0500

    mods

--- example1/views.py
+++ example1/views.py
@@ -3,9 +3,14 @@ from django.http import JsonResponse
 from rest_framework.permissions import AllowAny
 import logging
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views import APIView
 # Create your views here.
 
 class HelloWorld(APIView):
     @csrf_exempt
     def post(self, _):
+        return JsonResponse({'message' : 'Hello, success!'}, status=200)
+    
+    @csrf_exempt
+    def get(self, _):
         return JsonResponse({'message' : 'Hello, success!'}, status=200)
